1b83b6306529514b0274989eade1c43679e8edbb
Yandre13 <yandre13.elsf@gmail.com>
2019-07-13 01:39:49 -0500

    what a beauty

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,106 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Line } from "react-chartjs-2"
-import {data1, options} from './variables'
+import { optionsLine } from "./variables"
 
-export default ()=>{
+export default () => {
+  const [state, setState] = useState({
+    data1: [10, 0, 0, 0, 0, 0, 0],
+    data2: [10, 0, 0, 0, 0, 0, 0]
+  })
 
+  const getData = canvas => {
+    let ctx = canvas.getContext("2d")
 
- 
- 
+    let gradientStroke1 = ctx.createLinearGradient(1, 230, 0, 50),
+      gradientStroke2 = ctx.createLinearGradient(1, 210, 0, 50)
 
- 
+    gradientStroke1.addColorStop(1, "rgba(29,140,248,0.2)")
+    gradientStroke1.addColorStop(1, "rgba(29,140,248,0.0)")
+    gradientStroke1.addColorStop(0, "rgba(29,140,248,0)")
+
+    gradientStroke2.addColorStop(1, "rgba(122,255,51,0.1)")
+    gradientStroke2.addColorStop(1, "rgba(122,255, 51,0.0)")
+    gradientStroke2.addColorStop(0, "rgba(122,255,51,0)")
+
+    return {
+      labels: [
+        "Sunday",
+        "Monday",
+        "Twesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+      ],
+      datasets: [
+        {
+          label: "Videos",
+          borderColor: "hsl(150,40%,20%)",
+          pointBorderColor: "hsl(150,40%,40%)",
+          pointBackgroundColor: "hsl(150,40%,20%)",
+          pointBorderWidth: 2,
+          pointHoverRadius: 4,
+          pointHoverBorderWidth: 1,
+          backgroundColor: gradientStroke2,
+          data: state.data1,
+          borderWidth: 2
+        },
+        {
+          label: "Snaps",
+          borderColor: "hsl(220,60%,20%)", // line -------- and []
+          pointBorderColor: "hsl(220,60%,50%)", // (.)
+          pointBackgroundColor: "hsl(220,60%,20%)", //point (.)
+          pointBorderWidth: 2,
+          pointHoverRadius: 4,
+          pointHoverBorderWidth: 1,
+          backgroundColor: gradientStroke1,
+          data: state.data2,
+          borderWidth: 2
+        }
+      ]
+    }
+  }
+
+  useEffect(()=>{
+   setState({...state,
+    data1: [24, 25, 1, 10, 32, 2, 12],
+    data2: [14, 15, 21, 0, 12, 24, 22]
+   })
+  },[])
+
   return (
-   <>
-   <h3>Chart first {'<3'}</h3>
-   <div style={{width:700, height:250, backgroundColor: 'hsl(280, 0%,10%)'}}>
-    <Line options={options}
-     data={data1.data} />
-   </div>
-  </>
+    <>
+      <h3 style={{fontFamily: 'sans'}}>Chart first {"<3"}</h3>
+      <div
+        style={{ width: 700, height: 280, backgroundColor: "hsl(280, 0%,10%)" }}
+      >
+        <Line options={optionsLine} data={getData} />
+      </div>
+      <button
+       onClick={e=>{
+        let arr = []
+        state.data1.forEach((el,i)=>{
+         el = el+ 10
+         arr.push(el)
+        })
+        setState({...state, data1:arr})
+       }}
+      >
+       Update
+      </button>
+      --
+      <button
+       onClick={e=>{
+        let arr = []
+        state.data1.forEach((el,i)=>{
+         el = el- 10
+         arr.push(el)
+        })
+        setState({...state, data1:arr})
+       }}
+      >
+       Previuos
+      </button>
+    </>
   )
- 
-  
-
 }
--- src/variables/index.js
+++ src/variables/index.js
@@ -1,50 +1,4 @@
-const data1 = {
- data: canvas => {
-   let ctx = canvas.getContext("2d");
-
-   let gradientStroke1 = ctx.createLinearGradient(1, 230, 0, 50),gradientStroke2 = ctx.createLinearGradient(1, 210, 0, 50);
-
-   gradientStroke1.addColorStop(1, "rgba(29,140,248,0.2)");
-   gradientStroke1.addColorStop(1, "rgba(29,140,248,0.0)");
-   gradientStroke1.addColorStop(0, "rgba(29,140,248,0)");
-
-   gradientStroke2.addColorStop(1, "rgba(122,255,51,0.1)");
-   gradientStroke2.addColorStop(1, "rgba(122,255, 51,0.0)");
-   gradientStroke2.addColorStop(0, "rgba(122,255,51,0)");
-
-   return {
-     labels: ["1", "2", "3", "4", "5", "6"],
-     datasets: [
-       {
-         label: "Videos",
-         borderColor: "hsl(150,40%,20%)",
-         pointBorderColor: "hsl(150,40%,40%)",
-         pointBackgroundColor: "hsl(150,40%,20%)",
-         pointBorderWidth: 2,
-         pointHoverRadius: 4,
-         pointHoverBorderWidth: 1,
-         backgroundColor: gradientStroke2,
-         data: [24, 25, 1, 10, 32, 2, 12],
-         borderWidth: 2
-       },
-       {
-         label: "Snaps",
-         borderColor: "hsl(220,60%,20%)", // line -------- and []
-         pointBorderColor: "hsl(220,60%,50%)", // (.)
-         pointBackgroundColor: "hsl(220,60%,20%)", //point (.)
-         pointBorderWidth: 2,
-         pointHoverRadius: 4,
-         pointHoverBorderWidth: 1,
-         backgroundColor: gradientStroke1,
-         data: [14, 15, 21, 0, 12, 24, 22],
-         borderWidth: 2
-       }
-     ]
-   };
- }
-};
-
-const options = {
+const optionsLine = {
   maintainAspectRatio: false,
   legend: {
     display: true
@@ -92,8 +46,8 @@ const options = {
     ]
   },
   layout: {
-    padding: { left: 0, right: 0, top: 15, bottom: 15 }
+    padding: { left: 5, right: 35, top: 15, bottom: 15 }
   }
 };
 
-export { data1, options };
+export {optionsLine};
